320553da1a4389fb9104e847eed6ebd517637bb7
wesleynepo <wesleynepomuceno1@gmail.com>
2022-12-06 23:15:03 -0300

    solve day 6 first step

---
+++ day-06/a-solve.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+
+func main() {
+    input, _ := os.Open("./input")
+    defer input.Close()
+
+    buffer := bufio.NewScanner(input)
+
+    var packets int
+
+    for buffer.Scan() {
+        line := buffer.Text()
+
+        var found bool
+
+        for i := 0; i < len(line); i++ {
+            keys := make(map[string]bool)
+            found = false
+
+            for _, rune := range line[i:i+4] {
+                letter := string(rune)
+
+                if(keys[letter]) {
+                    found = true
+                    break
+                }
+
+                keys[letter] = true
+            }
+
+            if(!found) {
+                packets += (i + 4)
+                break
+            }
+        }
+    }
+
+
+    fmt.Println(packets)
+}
