a45c423cc814752c328cfbcf08c26f932df392e5
wesleynepo <wesleynepomuceno1@gmail.com>
2022-12-14 00:37:53 -0300

    finally solving day 08 step 1

---
+++ day-08/a-solve.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+
+func main() {
+    input, _ := os.Open("./input")
+    defer input.Close()
+
+    buffer := bufio.NewScanner(input)
+
+    grid := make([][]int, 0)
+
+    for buffer.Scan() {
+        line := buffer.Text()
+        row := strings.Split(line, "")
+        integers := convertRow(row)
+        grid = append(grid, integers)
+    }
+
+    rowLength := len(grid[0])
+    columnLength := len(grid)
+    initial := 2 * rowLength + 2 * (columnLength - 2)
+
+    calculate(grid, transpose(grid), &initial)
+    fmt.Println(initial)
+}
+
+func calculate(matrix [][]int, transposed [][]int, sum *int) {
+    rowLength, columnLength := lengths(matrix)
+
+    for i := 1; i < columnLength - 1; i++ {
+        for j := 1; j < rowLength - 1; j++ {
+            actual := matrix[i][j]
+            leftSlice := matrix[i][0:j]
+            rightSlice := matrix[i][j+1:]
+            upSlice := transposed[j][0:i]
+            downSlice := transposed[j][i+1:]
+            if maxSlice(leftSlice) < actual || maxSlice(rightSlice) < actual || maxSlice(upSlice) < actual || maxSlice(downSlice) < actual{
+                *sum++
+            }
+        }
+    }
+}
+
+func maxSlice(slice []int) int {
+    max := slice[0]
+    for _, e := range slice {
+        if e > max {
+            max = e
+        }
+    }
+    return max
+}
+
+func lengths(matrix [][]int) (int, int) {
+    return len(matrix[0]), len(matrix)
+}
+
+func convertRow(row []string) []int {
+    rowConverted := make([]int, 0)
+    for _, value := range row {
+        integer, _ := strconv.Atoi(value)
+        rowConverted = append(rowConverted, integer)
+    }
+
+    return rowConverted
+}
+
+func transpose(matrix [][]int) [][]int {
+    rowLength, columnLength := lengths(matrix)
+    transposed := make([][]int, rowLength)
+    for i := range transposed {
+        transposed[i] = make([]int, columnLength)
+    }
+
+    for i := 0; i < rowLength; i++ {
+        for j := 0; j < columnLength; j++ {
+            transposed[i][j] = matrix[j][i]
+        }
+    }
+
+    return transposed
+}
